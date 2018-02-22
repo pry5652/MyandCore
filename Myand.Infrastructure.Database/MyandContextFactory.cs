8b8f29c180fc13ccb68baa36192c01469db88a0a
Nurul Hadi <n.hadi1996@gmail.com>
2018-02-22 08:39:56 +0700

    Add MyandContextFactory for Init Migration

---
+++ Myand.Infrastructure.Database/MyandContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myand.Infrastructure.Database
+{
+	public class MyandContextFactory : IDesignTimeDbContextFactory<MyandContext>
+	{
+		public MyandContextFactory()
+		{
+		}
+
+
+		public MyandContext CreateDbContext(string[] args)
+		{
+			var builder = new DbContextOptionsBuilder<MyandContext>();
+			builder.UseSqlServer(
+				"Server=(localdb)\\mssqllocaldb;Database=myand;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+			return new MyandContext(builder.Options);
+		}
+	}
+}
